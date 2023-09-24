8781badb3d345ce025af838098dcf469a701a864
mawais <awaisakram2271@gmail.com>
2023-09-24 04:13:02 -0700

    bulb comit

---
+++ Script.js
@@ -0,0 +1,11 @@
+
+
+function bulbon (){
+let bulbobj=document.querySelector(".bulboff");
+bulbobj.setAttribute("src", "./on.png")
+}
+
+function bulboff(){
+    let bulbobj=document.querySelector(".bulbon");
+bulbobj.setAttribute("src", "./of.png")
+}
